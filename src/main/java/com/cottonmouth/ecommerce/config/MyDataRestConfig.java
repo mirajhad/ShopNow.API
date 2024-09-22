e4e67848c2d213302294123b2dce0934934fb39c
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2024-09-22 21:17:57 +0530

    reafactor cors

--- src/main/java/com/cottonmouth/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/cottonmouth/ecommerce/config/MyDataRestConfig.java
@@ -7,6 +7,7 @@ import com.cottonmouth.ecommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -20,6 +21,10 @@ import java.util.Set;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
+
     private EntityManager entityManager;
 
     @Autowired
@@ -28,7 +33,8 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     }
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST,
+                HttpMethod.DELETE, HttpMethod.PATCH};
 
 
         // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
@@ -39,6 +45,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         //call an internal helper method
         exposeIds(config);
+
+        //config cors mapping
+        cors.addMapping( config.getBasePath()+"/**").allowedOrigins(theAllowedOrigins);
 
     }
 
--- src/main/java/com/cottonmouth/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/cottonmouth/ecommerce/controller/CheckoutController.java
@@ -5,7 +5,7 @@ import com.cottonmouth.ecommerce.dto.PurchaseResponse;
 import com.cottonmouth.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
+
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
